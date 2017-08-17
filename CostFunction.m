b6db994393381ecbcf40b819796fa7150fc00756
Gaurav Parmar <gparmar@ucsd.edu>
2017-08-17 13:27:05 +0700

    fully fuctional neural net with 1 hidden layer of 25 nodes

--- CostFunction.m
+++ CostFunction.m
@@ -1,38 +1,57 @@
-function [J grad] = nnCostFunction(theta, A_size, B_size, Z, X, y, lambda)
-
+function [J grad] = CostFunction(theta, ...
+                                   n, ...
+                                   hidden_layer_size, ...
+                                   Z, ...
+                                   X, y, lambda)
 m = size(X,1);
-n = size(X,2);
 
 % Computing back the values of θ1 θ2 θ3;
-Theta1 = reshape(theta(1:((n+1)*A_size)), A_size, (n+1));
-Theta2 = reshape(theta(((n+1)*A_size)+1:((A_size+1)*B_size)), B_size, (A_size+1));
-Theta3 = reshape(theta(((A_size+1)*B_size))+1:((B_size+1)*Z)), Z, (B_size+1));
+t1start = 1;
+t1end = (n+1)*hidden_layer_size;
+t2start = t1end + 1;
+t2end = t2start -1 + (hidden_layer_size+1)*Z;
+Theta1 = reshape(theta(1:t1end), hidden_layer_size, (n+1));
+Theta2 = reshape(theta(t2start:t2end), Z, (hidden_layer_size+1));
 
 
 % Forward propogation to calculate H(θ)
 a1 = [ones(m,1) X];
 z2 = a1*Theta1';
 a2 = sigmoid(z2);
-a2 = [ones(m,1) a2];
+a2 = [ones(size(a2,1),1) a2];
 z3 = a2*Theta2';
-a3 = [ones(m,1) a3];
-z4 = a3*Theta3';
-a4 = [ones(m,1) a4];
-H = a4;
-
+a3 = sigmoid(z3);
+H = a3;
+J = 0;
 
 % Computing the cost, i.e. J(θ)
 for k = 1:Z
 	y_k = y == k;
-	h_k = H(:,k);
-	temp = sum(-y_k.*log(h_k)  -  (1-y_k).*log(1-h_k));
-	J = J + temp;
+	H_k = H(:,k);
+	temp = sum( (-y_k .* log(H_k)) - (1-y_k).*log(1-H_k));
+	J = J + temp/m;
 end
+
 reg_term = lambda/(2*m) * (sum(sum(Theta1(:,2:end).^2))  + sum(sum(Theta2(:,2:end).^2)));
 J = J + reg_term;
 
 
 % Backpropogation to compute the partial derivative terms
-
-
+D1 = zeros(size(Theta1));
+D2 = zeros(size(Theta2));
+for i = 1:m
+	for j = 1:Z
+		y_k = y(i)==j;
+		d3(j) = H(i,j) - y_k;
+	end
+    d2 = Theta2' * d3' .* sigmoidGradient([1, z2(i, :)])';
+    d2 = d2(2:end);
+    D1 = D1 + d2 * a1(i, :);
+    D2 = D2 + d3' * a2(i, :);
+end
+D1 = D1/m;
+D2 = D2/m;
+D1(:, 2:end) = D1(:, 2:end) + lambda / m * Theta1(:, 2:end);
+D2(:, 2:end) = D2(:, 2:end) + lambda / m * Theta2(:, 2:end);
+grad = [D1(:) ; D2(:)];
 end
--- main.m
+++ main.m
@@ -24,7 +24,7 @@ D = image(div);
 
 % PART 2: Forming the X and the Y matrix
 X = zeros(45 ,400);
-Y = zeros(400, 1);
+Y = zeros(45, 1);
 for i = 1:15
 	X(i,:) = P(i,:);
 	Y(i) = 1;
@@ -38,15 +38,17 @@ end
 
 % PART 3: Initializing parameter theta with random values and unrolling them
 INIT_EPSILON = 0.12;
-Theta1 = rand(4,401) * (2 * INIT_EPSILON) - INIT_EPSILON;
-Theta2 = rand(4,5) * (2*INIT_EPSILON) - INIT_EPSILON;
-Theta3 = rand(3,5) * (2 * INIT_EPSILON) - INIT_EPSILON;
-theta = [Theta1(:) ; Theta2(:); Theta3];
+Theta1 = rand(25,401) * (2 * INIT_EPSILON) - INIT_EPSILON;
+Theta2 = rand(3,26) * (2*INIT_EPSILON) - INIT_EPSILON;
+theta = [Theta1(:); Theta2(:)];
 
-
-
-% PART 4:
-
-
+% PART 4: Gradient Descent to find optimal theta values
+options = optimset('MaxIter', 500);
+lambda = 0;
+costFunction = @(p) CostFunction(p, ...
+                                   400, ...
+                                   25, ...
+                                   3, X, Y, lambda);
+[nn_params, cost] = fmincg(costFunction, theta, options);
 % end of the program
 end
